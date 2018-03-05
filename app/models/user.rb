fb2e4ef58f693db436fce2945e89f7ad2a5e4a9a
Luke Bartholomew <lukesbart@gmail.com>
2018-03-05 03:08:56 +0100

    Finish toy app

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  has_many :microposts
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }, presence: true
+  validates :name, length: { maximum: 15 }, presence: true
+
+end
